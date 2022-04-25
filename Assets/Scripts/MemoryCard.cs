e4a69d198f982f0d78447e640ccbe2acd3f428f0
Eric Ma <ma.eric11214@outlook.com>
2022-04-25 10:36:55 -0400

    chapter 5

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    public void OnMouseDown() {
+        Debug.Log("Testing 1 2 3");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
